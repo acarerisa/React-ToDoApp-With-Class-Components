938f33dfccfab551455a4e2a0b9fad5e261ced0b
acarerisa <acarerisa84@gmail.com>
2021-03-25 23:50:21 +0300

    ders 2

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -4,11 +4,28 @@ export default class ToDo extends Component {
     state = {
         input:"",
     }
+    changeHadler=(e)=>{
+        this.setState({
+            input:e.target.value
+        })
+    }
+    submitHandler=(e)=>{
+        this.props.addToDo(this.state.input)
+        this.setState({
+            input:""
+        })
+        e.preventDefault()
+
+    }
+    
     render() {
         return (
-            <form>
-                <input/>
-                <button>Add</button>
+            <form onSubmit={this.submitHandler} >
+                <input type="text" 
+                value={this.state.input}
+                onChange={this.changeHadler}
+                />
+                <button type="submit" >Add</button>
             </form>
         )
     }
--- src/Components/ToDoApp.js
+++ src/Components/ToDoApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ToDo from './ToDo'
 import ToDoList from './ToDoList'
+import { v4 as uuidv4 } from 'uuid';
 
 export default class ToDoApp extends Component {
     state = {
@@ -9,10 +10,27 @@ export default class ToDoApp extends Component {
             {id:2, name:"task2", done:false}
         ]
     }
+
+    addToDo = (newInput)=>{
+       if(newInput === ""){
+           alert("Görev girmeyi unuttunuz!")
+       } else if(this.state.tasks.find(task=>task.name ===newInput)){
+           alert("Bunu daha önce girmiştin")
+       }else{
+           const newTask = {
+               id:uuidv4(),
+               name: newInput,
+               done:false
+           }
+           this.setState({
+               tasks:[...this.state.tasks,newTask]
+           })
+       }
+    }
     render() {
         return (
             <div>
-                <ToDo/>
+                <ToDo addToDo={this.addToDo}/>
               <ToDoList tasks = {this.state.tasks} />  
             </div>
         )
